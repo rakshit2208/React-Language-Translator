0b009e80a11730b198a841f8dab90ab0f923cb8b
rakshit2208 <yadavrakshit290@gmail.com>
2023-05-22 22:48:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next'
+import landing from './landing';
+
+// Contains the value and text for the options
+const languages = [
+	{ value: 'en', text: "Options" },
+	{ value: 'en', text: "English" },
+	{ value: 'hi', text: "Hindi" },
+	{ value: 'bn', text: "Bengali" },
+  {value: 'gu',text:"Gujarati"}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	// It is a hook imported from 'react-i18next'
+	const { t } = useTranslation();
+
+	const [lang, setLang] = useState('en');
+
+	// This function put query that helps to
+	// change the language
+	const handleChange = e => {
+		setLang(e.target.value);
+		let loc = "http://localhost:3000/";
+		window.location.replace(loc + "?lng=" + e.target.value);
+	}
+
+	return (
+		<div className="App">
+
+			// we are showing the value by using the
+			// keys we have created in the website
+			<h1>{t('welcome')}</h1>
+			<label>{t('choose')}</label>
+
+      <p>{t('content')}</p>
+
+			<select value={lang} onChange={handleChange}>
+				{languages.map(item => {
+					return (<option key={item.value}
+					value={item.value}>{item.text}</option>);
+				})}
+			</select>
+
+      <h3>
+       {t('name')} : RKy
+       
+     </h3>
+
+    
+		</div>
+	);
 }
 
 export default App;
---
+++ src/landing.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+const Landing = () => {
+
+    const { t } = useTranslation();
+
+  return (
+    <div>
+
+     
+       
+            
+    </div>
+  )
+}
+
+export default Landing
